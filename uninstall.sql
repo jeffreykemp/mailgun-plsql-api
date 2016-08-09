7334a17df06abbc73797a6f1a1eb0d6ce7741af3
jeffrey kemp <jeffrey.kemp@jk64.com>
2016-08-09 20:59:58 +0800

    release 0.6 - more objects to drop

--- uninstall.sql
+++ uninstall.sql
@@ -1,10 +1,16 @@
 prompt uninstall.sql
--- v0.4
+-- v0.6
 
-prompt drop job
+prompt drop job (process queue)
 begin dbms_scheduler.stop_job ('mailgun_process_queue'); exception when others then if sqlcode not in (-27366,-27475) then raise; end if; end;
 /
 begin dbms_scheduler.drop_job('mailgun_process_queue'); exception when others then if sqlcode not in (-27366,-27475) then raise; end if; end;
+/
+
+prompt drop job (purge logs)
+begin dbms_scheduler.stop_job ('mailgun_purge_logs'); exception when others then if sqlcode not in (-27366,-27475) then raise; end if; end;
+/
+begin dbms_scheduler.drop_job('mailgun_purge_logs'); exception when others then if sqlcode not in (-27366,-27475) then raise; end if; end;
 /
 
 prompt drop queue
@@ -15,20 +21,38 @@ begin dbms_aqadm.drop_queue (user||'.mailgun_queue'); exception when others then
 begin dbms_aqadm.drop_queue_table (user||'.mailgun_queue_tab'); exception when others then if sqlcode not in (-24010,-24002) then raise; end if; end;
 /
 
-prompt drop table
+prompt drop tables
 begin execute immediate 'drop table mailgun_email_log'; exception when others then if sqlcode!=-942 then raise; end if; end;
+/
+begin execute immediate 'drop table mailgun_settings'; exception when others then if sqlcode!=-942 then raise; end if; end;
 /
 
 prompt drop types
-begin execute immediate 'drop type t_mailgun_email'; exception when others then if sqlcode!=-4043 then raise; end if; end;
-/
 begin execute immediate 'drop type t_mailgun_attachment_arr'; exception when others then if sqlcode!=-4043 then raise; end if; end;
 /
 begin execute immediate 'drop type t_mailgun_recipient_arr'; exception when others then if sqlcode!=-4043 then raise; end if; end;
 /
+begin execute immediate 'drop type t_mailgun_event_arr'; exception when others then if sqlcode!=-4043 then raise; end if; end;
+/
+begin execute immediate 'drop type t_mailgun_stat_arr'; exception when others then if sqlcode!=-4043 then raise; end if; end;
+/
+begin execute immediate 'drop type t_mailgun_suppression_arr'; exception when others then if sqlcode!=-4043 then raise; end if; end;
+/
+begin execute immediate 'drop type t_mailgun_tag_arr'; exception when others then if sqlcode!=-4043 then raise; end if; end;
+/
+begin execute immediate 'drop type t_mailgun_email'; exception when others then if sqlcode!=-4043 then raise; end if; end;
+/
 begin execute immediate 'drop type t_mailgun_attachment'; exception when others then if sqlcode!=-4043 then raise; end if; end;
 /
 begin execute immediate 'drop type t_mailgun_recipient'; exception when others then if sqlcode!=-4043 then raise; end if; end;
+/
+begin execute immediate 'drop type t_mailgun_event'; exception when others then if sqlcode!=-4043 then raise; end if; end;
+/
+begin execute immediate 'drop type t_mailgun_stat'; exception when others then if sqlcode!=-4043 then raise; end if; end;
+/
+begin execute immediate 'drop type t_mailgun_suppression'; exception when others then if sqlcode!=-4043 then raise; end if; end;
+/
+begin execute immediate 'drop type t_mailgun_tag'; exception when others then if sqlcode!=-4043 then raise; end if; end;
 /
 
 prompt drop package
